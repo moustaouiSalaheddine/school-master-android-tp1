38364fbcac983c7a9a4555f0d377e928e5fb4ddb
moustaouiSalaheddine <moustaoui.salaheddine@gmail.com>
2024-10-11 19:59:58 +0100

    add Lifecycle

--- app/src/main/java/com/example/application01/ActivitePrincipale.java
+++ app/src/main/java/com/example/application01/ActivitePrincipale.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
 
 public class ActivitePrincipale extends AppCompatActivity {
 
@@ -15,10 +16,60 @@ public class ActivitePrincipale extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        System.out.println("onCreate called");
+        Log.d("Lifecycle", "onCreate called");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart called");
+        Log.d("Lifecycle", "onStart called");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("onStop called");
+        Log.d("Lifecycle","onStop called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("onRestart called");
+        Log.d("Lifecycle","onRestart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume called");
+        Log.d("Lifecycle","onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause called");
+        Log.d("Lifecycle","onPause called");
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("onSaveInstanceState called");
+        Log.d("Lifecycle","onSaveInstanceState called");
+        // Save additional state if necessary
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        System.out.println("onRestoreInstanceState called");
+        Log.d("Lifecycle","onRestoreInstanceState called");
+        // Restore additional state if necessary
+    }
+
 }
